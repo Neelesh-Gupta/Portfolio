dc9e496d6287ddeb5d1edef4c6ca7516aff77361
Neelesh-Gupta <130556927+Neelesh-Gupta@users.noreply.github.com>
2026-07-13 03:43:50 +0530

    Updated Contents

--- src/constants/glass.js
+++
@@ -1,4 +0,0 @@
-export const GLASS =
-  'glass rounded-3xl shadow-[0_8px_32px_0_rgba(0,0,0,0.3)]'
-
-export const GLASS_BENTO = 'glass rounded-2xl'
--- src/sections/Hero.jsx
+++
@@ -1,7 +0,0 @@
-function Hero() {
-  return (
-    <section id="home" className="relative min-h-screen" aria-hidden="true" />
-  )
-}
-
-export default Hero
